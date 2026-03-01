ab80b8363dc212e89963999cb1ecf35aa593f7ec
YeoSeongil <seongil_yeo@naver.com>
2026-03-02 01:00:28 +0900

    [SETTING] Create Core Module

---
+++ Projects/Core/Project.swift
@@ -0,0 +1,24 @@
+//
+//  Project.swift
+//  Manifests
+//
+//  Created by 여성일 on 3/1/26.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+  name: Targets.core,
+  targets: [
+    .target(
+      name: Targets.core,
+      destinations: .iOS,
+      product: .framework,
+      bundleId: "com.yeo.flyleaf.core",
+      sources: ["Sources/**"],
+      dependencies: []
+    )
+  ]
+)
+
