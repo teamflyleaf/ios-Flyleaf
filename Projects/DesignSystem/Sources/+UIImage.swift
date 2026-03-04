72d89593e65e46efb2d7bda57e1edacea3fee3e7
YeoSeongil <seongil_yeo@naver.com>
2026-03-05 01:57:08 +0900

    [#01] ui: 커스텀 아이콘 extension 구현
    앱에서 전역적으로 사용할 아이콘 UIImage Extension

---
+++ Projects/DesignSystem/Sources/+UIImage.swift
@@ -0,0 +1,28 @@
+//
+//  +UIImage.swift
+//  DesignSystem
+//
+//  Created by 여성일 on 3/5/26.
+//
+
+import UIKit
+
+public extension UIImage {
+  // MARK: - Neutral
+  static let book = DesignSystemAsset.book.image.withRenderingMode(.alwaysTemplate)
+  static let pen = DesignSystemAsset.pen.image.withRenderingMode(.alwaysTemplate)
+  static let plus = DesignSystemAsset.plus.image.withRenderingMode(.alwaysTemplate)
+  static let right = DesignSystemAsset.right.image.withRenderingMode(.alwaysTemplate)
+  static let search = DesignSystemAsset.search.image.withRenderingMode(.alwaysTemplate)
+  static let time = DesignSystemAsset.time.image.withRenderingMode(.alwaysTemplate)
+  static let xmark = DesignSystemAsset.x.image.withRenderingMode(.alwaysTemplate)
+  
+  // MARK: - Chevron
+  static let chevronLeft = DesignSystemAsset.chevronLeft.image.withRenderingMode(.alwaysTemplate)
+  static let chevronRight = DesignSystemAsset.chevronRight.image.withRenderingMode(.alwaysTemplate)
+
+  // MARK: - Airplane
+  static let airplane = DesignSystemAsset.airplane.image.withRenderingMode(.alwaysTemplate)
+  static let landing = DesignSystemAsset.landing.image.withRenderingMode(.alwaysTemplate)
+  static let takeOff = DesignSystemAsset.takeOff.image.withRenderingMode(.alwaysTemplate)
+}
