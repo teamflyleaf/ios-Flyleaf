f33e2be7e646bf6865f5e42561fb79dcf977ff59
YeoSeongil <seongil_yeo@naver.com>
2026-03-05 02:35:31 +0900

    [#01] ui: resized 메소드 퀵헬프 추가
    퀵헬프 주석 추가

--- Projects/DesignSystem/Sources/+UIImage.swift
+++ Projects/DesignSystem/Sources/+UIImage.swift
@@ -26,6 +26,22 @@ public extension UIImage {
   static let landing = DesignSystemAsset.landing.image.withRenderingMode(.alwaysTemplate)
   static let takeOff = DesignSystemAsset.takeOff.image.withRenderingMode(.alwaysTemplate)
   
+  // MARK: - Public Method
+  /// UIImage의 크기를 지정한 width, height로 리사이즈하여 새로운 이미지를 반환합니다.
+  ///
+  /// 기존 이미지의 `renderingMode`를 유지하여 template 이미지의 경우 `tintColor`가 정상적으로 적용됩니다.
+  ///
+  /// - Parameters:
+  ///   - width: 변경할 이미지의 너비 (pt 단위)
+  ///   - height: 변경할 이미지의 높이
+  /// - Returns: 지정한 크기로 리사이즈된 `UIImage`
+  ///
+  /// Example:
+  /// ```swift
+  /// let icon = UIImage.takeOff.resized(24, 24)
+  /// button.setImage(icon, for: .normal)
+  /// button.tintColor = .key0
+  /// ```
   func resized(_ width: Int, _ height: Int) -> UIImage {
     let size = CGSize(width: width, height: height)
     let rendered = UIGraphicsImageRenderer(size: size).image { _ in
