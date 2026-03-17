ffd5ccd95ce62d1b7d3c512a768e91e23983a458
YeoSeongil <seongil_yeo@naver.com>
2026-03-17 23:34:38 +0900

    [#20] feature: Ticket 구현

---
+++ Projects/DesignSystem/Sources/Components/Ticket/BarcodeView.swift
@@ -0,0 +1,65 @@
+//
+//  BarcodeView.swift
+//  DesignSystem
+//
+//  Created by 여성일 on 3/16/26.
+//
+
+import UIKit
+
+/// 간단한 바코드 형태를 그리는 커스텀 뷰입니다.
+///
+/// ```swift
+/// let barcodeView = BarcodeView()
+/// ```
+///
+/// - Note:
+///   - `draw(_:)`를 사용하여 Core Graphics로 직접 렌더링합니다.
+///   - 뷰의 width에 맞춰 패턴이 반복됩니다.
+final class BarcodeView: BaseView {
+  override func draw(_ rect: CGRect) {
+    // Core Graphics Context 불러오기
+    guard let ctx = UIGraphicsGetCurrentContext() else { return }
+    
+    // 막대 색상 설정
+    ctx.setFillColor(UIColor.n0.cgColor)
+    
+    // 바코드 패턴(막대 너비)
+    // 값이 더 클수록 더 두꺼운 막대
+    // TODO: 랜덤 바코드를 위한 랜덤 패턴으로 리팩토링 가능성 생각해보기
+    let pattern: [CGFloat] = [
+      3, 1, 5, 2, 4, 2, 6, 1, 3, 2
+    ]
+    
+    // 막대 사이 간격
+    let spacing: CGFloat = 4
+    
+    // 현재 바코드 그릴 x좌표
+    var x: CGFloat = 0
+    
+    // 패턴 인덱스 (반복을 위한 인덱스 값임.)
+    var index = 0
+    
+    // 뷰 너비 끝까지 반복해서 바코드를 채움.
+    while x < rect.width {
+      
+      // 현재 패턴에 해당하는 막대 너비
+      let width = pattern[index % pattern.count]
+      
+      // 세로 막대 그리기
+      ctx.fill(
+        CGRect(
+          x: x,
+          y: 0,
+          width: width,
+          height: rect.height
+        )
+      )
+      // 다음 위치로 이동 (막대 너비 + 간격)
+      x += width + spacing
+      
+      // 다음 패턴으로 이동
+      index += 1
+    }
+  }
+}
---
+++ Projects/DesignSystem/Sources/Components/Ticket/TicketView.swift
@@ -0,0 +1,169 @@
+//
+//  TicketView.swift
+//  DesignSystem
+//
+//  Created by 여성일 on 3/16/26.
+//
+
+import Core
+import UIKit
+import SnapKit
+import Then
+
+/// 탑승권(티켓) 뷰 입니다.
+///
+/// 책 정보, 공항 정보, 페이지 수, 이동 거리, 바코드 영역을 포함한 탑승권 UI입니다.
+///
+/// ```swift
+/// let ticketView = TicketView()
+/// ticketView.configure(
+///   bookItem: book,
+///   departure: departureAirport,
+///   destination: destinationAirport
+/// )
+/// ```
+public final class TicketView: BaseView {
+  // MARK: - UI
+  private let shapeView = TicketShapeView()
+  
+  private let logoView = UILabel().then {
+    $0.text = "Flyleaf"
+    $0.font = .c3
+    $0.textColor = .key0
+    $0.textAlignment = .center
+    $0.backgroundColor = .n50
+    $0.layer.cornerRadius = 12
+    $0.clipsToBounds = true
+  }
+  
+  private let routeBookInfoView = RouteBookInfoView()
+  
+  private let startTitleLabel = UILabel().then {
+    $0.font = .c3
+    $0.text = "출발"
+    $0.textColor = .n0
+    $0.textAlignment = .left
+  }
+  
+  private let startPageLabel = UILabel().then {
+    $0.font = .h4_sb
+    $0.text = "0p"
+    $0.textColor = .n0
+  }
+  
+  private let arriveTitleLabel = UILabel().then {
+    $0.font = .c3
+    $0.text = "도착"
+    $0.textColor = .n0
+    $0.textAlignment = .center
+  }
+  
+  private let arrivePageLabel = UILabel().then {
+    $0.font = .h4_sb
+    $0.textColor = .n0
+    $0.textAlignment = .center
+  }
+  
+  private let distanceTitleLabel = UILabel().then {
+    $0.font = .c3
+    $0.text = "거리"
+    $0.textColor = .n0
+    $0.textAlignment = .right
+  }
+  
+  private let distanceLabel = UILabel().then {
+    $0.font = .h4_sb
+    $0.textColor = .n0
+    $0.textAlignment = .right
+  }
+  
+  private let barcodeView = BarcodeView()
+
+  public override func configureUI() {
+    addSubview(shapeView)
+
+    [
+      logoView,
+      routeBookInfoView,
+      startTitleLabel,
+      startPageLabel,
+      arriveTitleLabel,
+      arrivePageLabel,
+      distanceTitleLabel,
+      distanceLabel,
+      barcodeView
+    ].forEach {
+      shapeView.addSubview($0)
+    }
+  }
+
+  public override func setupLayout() {
+    shapeView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+    
+    logoView.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(10)
+      $0.leading.equalToSuperview().offset(20)
+      $0.height.equalTo(24)
+      $0.width.equalTo(55)
+    }
+    
+    routeBookInfoView.snp.makeConstraints {
+      $0.top.equalTo(logoView.snp.bottom).offset(6)
+      $0.horizontalEdges.equalToSuperview().inset(20)
+    }
+    
+    startTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(routeBookInfoView.snp.bottom).offset(20)
+      $0.leading.equalToSuperview().offset(20)
+    }
+    
+    startPageLabel.snp.makeConstraints {
+      $0.top.equalTo(startTitleLabel.snp.bottom).offset(4)
+      $0.leading.equalToSuperview().offset(20)
+    }
+    
+    arriveTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(routeBookInfoView.snp.bottom).offset(20)
+      $0.centerX.equalToSuperview()
+    }
+    
+    arrivePageLabel.snp.makeConstraints {
+      $0.top.equalTo(arriveTitleLabel.snp.bottom).offset(4)
+      $0.centerX.equalToSuperview()
+    }
+        
+    distanceTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(routeBookInfoView.snp.bottom).offset(20)
+      $0.trailing.equalToSuperview().inset(20)
+    }
+    
+    distanceLabel.snp.makeConstraints {
+      $0.top.equalTo(distanceTitleLabel.snp.bottom).offset(4)
+      $0.trailing.equalToSuperview().inset(20)
+    }
+
+    barcodeView.snp.makeConstraints {
+      $0.horizontalEdges.equalToSuperview().inset(20)
+      $0.bottom.equalToSuperview().inset(24)
+      $0.height.equalTo(64)
+    }
+  }
+
+  // MARK: - Public Method
+  public func configure(
+    bookItem: BookInfo,
+    departure: AirportInfo,
+    destination: AirportInfo
+  ) {
+    routeBookInfoView.configure(
+      bookItem: bookItem,
+      departure: departure,
+      destination: destination
+    )
+
+    arrivePageLabel.text = "\(bookItem.itemPage)p"
+    distanceLabel.text = "\(AirportInfo.distanceKm(from: departure, to: destination).formattedWithComma)km"
+  }
+}
