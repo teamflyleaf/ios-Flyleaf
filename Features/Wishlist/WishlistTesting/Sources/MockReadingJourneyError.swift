5583f4f039b352966036cb50bf6ec114c14b44dd
YeoSeongil <seongil_yeo@naver.com>
2026-03-18 01:51:41 +0900

    [#20] Test: WishTicketViewModel 단위테스팅

---
+++ Features/Wishlist/WishlistTesting/Sources/MockReadingJourneyError.swift
@@ -0,0 +1,23 @@
+//
+//  MockReadingJourneyError.swift
+//  Wishlist
+//
+//  Created by 여성일 on 3/18/26.
+//
+
+import Foundation
+
+enum MockReadingJourneyError: Error {
+  case failed
+}
+
+enum MockLocalizedReadingJourneyError: LocalizedError {
+  case custom
+
+  var errorDescription: String? {
+    switch self {
+    case .custom:
+      return "중복된 독서 여행입니다."
+    }
+  }
+}
