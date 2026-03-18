c624e67996b2c2cf37e8fa1e839699b65e862a1e
YeoSeongil <seongil_yeo@naver.com>
2026-03-18 22:33:01 +0900

    [#20] feature: 예제앱

--- Features/History/HistoryExample/Sources/SceneDelegate.swift
+++ Features/History/HistoryExample/Sources/SceneDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import HistoryFeature
+import HistoryInterface
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -21,7 +22,10 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = scene as? UIWindowScene else { return }
     
     let window = UIWindow(windowScene: windowScene)
-    window.rootViewController = HistoryRootViewController()
+    let viewModel = RegisterHistoryViewModel()
+    let registerHistoryBuilder: RegisterHistoryBuildable = RegisterHistoryBuilder()
+    let registerHistoryVC = registerHistoryBuilder.build()
+    window.rootViewController = registerHistoryVC
     window.makeKeyAndVisible()
     
     self.window = window
