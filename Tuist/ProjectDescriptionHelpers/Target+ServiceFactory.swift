f5d331ffa570575a8b03e45b02a09d35c103bd5b
YeoSeongil <seongil_yeo@naver.com>
2026-04-07 23:26:30 +0900

    [#70] tuist: Service 모듈 헬퍼 정의

---
+++ Tuist/ProjectDescriptionHelpers/Target+ServiceFactory.swift
@@ -0,0 +1,82 @@
+//
+//  Target+ServiceFactory.swift
+//  Manifests
+//
+//  Created by 여성일 on 4/7/26.
+//
+
+import ProjectDescription
+
+public extension Target {
+  static func serviceImplementation(
+    _ name: ServiceName,
+    dependencies: [TargetDependency] = []
+  ) -> Target {
+    let base = name.rawValue
+    return .target(
+      name: Targets.service(name, .implementation),
+      destinations: .iOS,
+      product: .framework,
+      bundleId: "com.yeo.flyleaf.\(base.lowercased()).implementation",
+      sources: ["\(base)Implementation/Sources/**"],
+      dependencies: [
+        .service(name, .interface),
+        .core()
+      ] + dependencies
+    )
+  }
+  
+  static func serviceInterface(
+    _ name: ServiceName,
+    dependencies: [TargetDependency] = []
+  ) -> Target {
+    let base = name.rawValue
+    return .target(
+      name: Targets.service(name, .interface),
+      destinations: .iOS,
+      product: .framework,
+      bundleId: "com.yeo.flyleaf.\(base.lowercased()).interface",
+      sources: ["\(base)Interface/Sources/**"],
+      dependencies: [
+        .core()
+      ] + dependencies
+    )
+  }
+  
+  static func serviceTesting(
+    _ name: ServiceName,
+    dependencies: [TargetDependency] = []
+  ) -> Target {
+    let base = name.rawValue
+    return .target(
+      name: Targets.service(name, .testing),
+      destinations: .iOS,
+      product: .framework,
+      bundleId: "com.yeo.flyleaf.\(base.lowercased()).testing",
+      sources: ["\(base)Testing/Sources/**"],
+      dependencies: [
+        .service(name, .interface),
+        .core()
+      ] + dependencies
+    )
+  }
+  
+  static func serviceTests(
+    _ name: ServiceName,
+    dependencies: [TargetDependency] = []
+  ) -> Target {
+    let base = name.rawValue
+    return .target(
+      name: Targets.service(name, .tests),
+      destinations: .iOS,
+      product: .unitTests,
+      bundleId: "com.yeo.flyleaf.\(base.lowercased()).tests",
+      sources: ["\(base)Tests/Tests/**"],
+      dependencies: [
+        .service(name, .implementation),
+        .service(name, .interface),
+        .service(name, .testing)
+      ] + dependencies
+    )
+  }
+}
