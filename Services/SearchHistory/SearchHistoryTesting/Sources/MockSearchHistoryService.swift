af485e22b54d4acdbdf18074f98672e19d968bc6
YeoSeongil <seongil_yeo@naver.com>
2026-04-08 00:12:15 +0900

    [#70] tests: SearchHistoryService 단위테스팅

--- Services/SearchHistory/SearchHistoryTesting/Sources/MockSearchHistoryService.swift
+++ Services/SearchHistory/SearchHistoryTesting/Sources/MockSearchHistoryService.swift
@@ -5,9 +5,26 @@
 //  Created by 여성일 on now.
 //
 
+import Core
 import Foundation
 import SearchHistoryInterface
 
 public final class MockSearchHistoryService: SearchHistoryServicing {
   public init() {}
+  
+  public func fetch(type: SearchType) -> [String] {
+    fatalError()
+  }
+  
+  public func save(_ query: String, type: SearchType) {
+    fatalError()
+  }
+  
+  public func delete(_ query: String, type: SearchType) {
+    fatalError()
+  }
+  
+  public func deleteAll(type: SearchType) {
+    fatalError()
+  }
 }
