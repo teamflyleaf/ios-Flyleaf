17136dbeb5ed5760f4221619c8b444a2b157395a
YeoSeongil <seongil_yeo@naver.com>
2026-04-07 23:59:31 +0900

    [#70] feature: SearchHistory Service 구현

--- Services/SearchHistory/SearchHistoryImplementation/Sources/SearchHistoryService.swift
+++ Services/SearchHistory/SearchHistoryImplementation/Sources/SearchHistoryService.swift
@@ -5,9 +5,49 @@
 //  Created by 여성일 on now.
 //
 
+import Core
 import Foundation
 import SearchHistoryInterface
 
 public final class SearchHistoryService: SearchHistoryServicing {
-  public init() {}
+  private let userDefaults: UserDefaults
+  private let maxCount: Int
+
+  public init(
+    userDefaults: UserDefaults = .standard,
+    maxCount: Int = 10
+  ) {
+    self.userDefaults = userDefaults
+    self.maxCount = maxCount
+  }
+
+  public func fetch(type: SearchType) -> [String] {
+    userDefaults.stringArray(forKey: type.recentSearchKey) ?? []
+  }
+
+  public func save(_ query: String, type: SearchType) {
+    let trimmedQuery = query.trimmingCharacters(in: .whitespacesAndNewlines)
+    guard !trimmedQuery.isEmpty else { return }
+
+    var items = fetch(type: type)
+
+    items.removeAll { $0 == trimmedQuery }
+    items.insert(trimmedQuery, at: 0)
+
+    if items.count > maxCount {
+      items = Array(items.prefix(maxCount))
+    }
+
+    userDefaults.set(items, forKey: type.recentSearchKey)
+  }
+
+  public func delete(_ query: String, type: SearchType) {
+    var items = fetch(type: type)
+    items.removeAll { $0 == query }
+    userDefaults.set(items, forKey: type.recentSearchKey)
+  }
+
+  public func deleteAll(type: SearchType) {
+    userDefaults.removeObject(forKey: type.recentSearchKey)
+  }
 }
--- Services/SearchHistory/SearchHistoryInterface/Sources/SearchHistoryServicing.swift
+++ Services/SearchHistory/SearchHistoryInterface/Sources/SearchHistoryServicing.swift
@@ -5,7 +5,12 @@
 //  Created by 여성일 on now.
 //
 
+import Core
 import Foundation
 
 public protocol SearchHistoryServicing {
+  func fetch(type: SearchType) -> [String]
+  func save(_ query: String, type: SearchType)
+  func delete(_ query: String, type: SearchType)
+  func deleteAll(type: SearchType)
 }
