7fbf418faff631c3d53a36b76b6f684e9f6989e8
YeoSeongil <seongil_yeo@naver.com>
2026-04-09 19:45:10 +0900

    [#75] feature: AirportSearch 서비스 구현

--- Services/AirportSearch/AirportSearchInterface/Sources/AirportSearchServicing.swift
+++ Services/AirportSearch/AirportSearchInterface/Sources/AirportSearchServicing.swift
@@ -5,7 +5,10 @@
 //  Created by 여성일 on now.
 //
 
+import Core
 import Foundation
 
 public protocol AirportSearchServicing {
+  func loadAirports() throws
+  func searchAirports(query: String) -> [AirportInfo]
 }
