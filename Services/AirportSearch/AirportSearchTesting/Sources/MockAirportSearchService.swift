948535ac2ddc42069d5444f48d179cb236d49c6f
YeoSeongil <seongil_yeo@naver.com>
2026-04-09 20:00:02 +0900

    [#75] tests: AirportSearchService 단위 테스트

--- Services/AirportSearch/AirportSearchTesting/Sources/MockAirportSearchService.swift
+++ Services/AirportSearch/AirportSearchTesting/Sources/MockAirportSearchService.swift
@@ -5,9 +5,18 @@
 //  Created by 여성일 on now.
 //
 
+import Core
 import Foundation
 import AirportSearchInterface
 
 public final class MockAirportSearchService: AirportSearchServicing {
   public init() {}
+  
+  public func loadAirports() throws {
+    fatalError()
+  }
+  
+  public func searchAirports(query: String) -> [AirportInfo] {
+    fatalError()
+  }
 }
