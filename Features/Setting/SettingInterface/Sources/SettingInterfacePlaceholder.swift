319a918f5be2c7e7a8ff0817bbf0924733766bc4
YeoSeongil <seongil_yeo@naver.com>
2026-04-21 16:46:37 +0900

    [#108] tuist: Setting Feature 모듈 생성

---
+++ Features/Setting/SettingInterface/Sources/SettingInterfacePlaceholder.swift
@@ -0,0 +1,8 @@
+//
+//  SettingInterfacePlaceholder.swift
+//  SettingInterface
+//
+//  Created by 여성일 on now.
+//
+
+public enum SettingInterfacePlaceholder {}
---
+++ Features/Setting/SettingTests/Tests/SettingFeatureTests.swift
@@ -0,0 +1,14 @@
+//
+//  Tests.swift
+//  SettingTests
+//
+//  Created by 여성일 on now.
+//
+
+import XCTest
+
+final class SettingFeatureTests: XCTestCase {
+  func test_example() {
+    XCTAssertTrue(true)
+  }
+}
