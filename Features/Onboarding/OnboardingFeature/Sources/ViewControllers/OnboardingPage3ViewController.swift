e2926e4614f837d9c02670bfa504df711ddddeea
YeoSeongil <seongil_yeo@naver.com>
2026-06-11 21:45:53 +0900

    [#126] feature: Lottie 적용

---
+++ Features/Onboarding/OnboardingFeature/Sources/ViewControllers/OnboardingPage3ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardingPage3ViewController.swift
+//  Onboarding
+//
+//  Created by 여성일 on 6/11/26.
+//
+
+import Core
+import UIKit
+import DesignSystem
+import Lottie
+import SnapKit
+import Then
+
+public final class OnboardingPage3ViewController: BaseViewController {
+  private let viewModel: OnboardingViewModel
+  
+  public init(viewModel: OnboardingViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  public required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - UI
+  private let titleLabel = UILabel().then {
+    $0.text = "여행은 페이지를 따라 이어져요"
+    $0.font = .h2
+    $0.textColor = .n0
+    $0.textAlignment = .left
+  }
+  
+  private let subtitleLabel = UILabel().then {
+    $0.text = "독서가 진행될수록\n여정도 함께 채워집니다."
+    $0.font = .c1
+    $0.textColor = .n20
+    $0.textAlignment = .left
+    $0.numberOfLines = 0
+  }
+  
+  private lazy var animationView: LottieAnimationView = {
+    let appBundle = Bundle(for: OnboardingPage3ViewController.self)
+    let bundle = appBundle.url(forResource: "Onboarding_OnboardingFeature", withExtension: "bundle")
+      .flatMap { Bundle(url: $0) } ?? appBundle
+    let view = LottieAnimationView(name: "world_map_animation", bundle: bundle)
+    view.loopMode = .loop
+    view.contentMode = .scaleAspectFit
+    view.animationSpeed = 1.7
+    return view
+  }()
+  
+  // MARK: - Configure
+  override public func configureUI() {
+    view.backgroundColor = .bg0
+
+    [
+      titleLabel,
+      subtitleLabel,
+      animationView
+    ].forEach {
+      view.addSubview($0)
+    }
+    
+    animationView.play()
+  }
+  
+  override public func setupLayout() {
+    titleLabel.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+      $0.horizontalEdges.equalToSuperview().inset(20)
+    }
+    
+    subtitleLabel.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+      $0.horizontalEdges.equalToSuperview().inset(20)
+    }
+    
+    animationView.snp.makeConstraints {
+      $0.top.equalTo(subtitleLabel.snp.bottom).offset(60)
+      $0.horizontalEdges.equalToSuperview()
+      $0.height.equalTo(200)
+    }
+  }
+  
+  // MARK: - Binding
+  public override func bind() {
+    
+  }
+}
+
