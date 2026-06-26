91a7c25d76b550de78c755a3b27dc701e9dca451
YeoSeongil <seongil_yeo@naver.com>
2026-06-26 22:34:07 +0900

    [#130] chore: 읽은 책 페이지, 메모 페이지 테마 적용

--- Features/History/HistoryFeature/Sources/ViewControllers/ReviewWriteViewController.swift
+++ Features/History/HistoryFeature/Sources/ViewControllers/ReviewWriteViewController.swift
@@ -51,7 +51,7 @@ public final class ReviewWriteViewController: BaseViewController {
   }
   
   private let contentContainerView = UIView().then {
-    $0.backgroundColor = .n50.withAlphaComponent(0.6)
+    $0.backgroundColor = .n20.withAlphaComponent(0.6)
     $0.layer.cornerRadius = 16
     $0.clipsToBounds = true
   }
@@ -67,7 +67,7 @@ public final class ReviewWriteViewController: BaseViewController {
   private let placeholderLabel = UILabel().then {
     $0.text = "인상 깊은 문장이나 생각을 남겨보세요"
     $0.font = .c2
-    $0.textColor = .n20
+    $0.textColor = .gray0
     $0.numberOfLines = 1
   }
   
